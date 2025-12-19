6682f7e1135461b653d3677a92afe02430fa4bfb
Прохоренко Софья <sofia.prokhorenko_1@mail.ru>
2025-12-19 14:21:11 +0300

    Update main_routes.py

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -8,6 +8,10 @@ import json
 
 main_bp = Blueprint('main', __name__)
 
+@main_bp.route('/')
+def index():
+    return render_template('base.html')
+
 @main_bp.route('/dashboard')
 @login_required
 def dashboard():
